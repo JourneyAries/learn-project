6ffd168905926391b84d465265a50fe4a210341e
JourneyAries <journeyaries13@gmail.com>
2025-04-05 22:53:52 +0700

    [prayer-app](v1):fetching 100%

--- prayer-app/src/App.jsx
+++ prayer-app/src/App.jsx
@@ -5,14 +5,23 @@ import { useEffect } from 'react';
 function App() {
 	const [input, setInput] = useState('');
 	const [term, setTerm] = useState('Jakarta');
-	const [data, setData] = useState({});
+	const [data, setData] = useState(null);
+
+	const prayerTime = ['Fajr', 'Dhuhr', 'Asr', 'Maghrib', 'Isha'];
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (input.trim()) {
+			setTerm(input.trim());
+			setInput('');
+		}
+	};
 
 	useEffect(() => {
 		fetch(
-			`https://api.aladhan.com/v1/timingsByAddress/01-01-2025?address=${term}`,
+			`https://api.aladhan.com/v1/timingsByAddress/${Date()}?address=${term}`,
 		)
 			.then((res) => res.json())
-			.then((res) => setData(res));
+			.then((res) => setData(res.data));
 	}, [term]);
 
 	return (
@@ -20,17 +29,12 @@ function App() {
 			<div className='flex flex-col w-full justify-between items-center gap-3 absolute top-5'>
 				<div className='w-full flex justify-between px-[70px] text-[#e3e6e3]'>
 					<div>
-						<div>Wednesday</div>
-						<div>25 Jan, 2024</div>
+						<div>{data?.date?.gregorian?.weekday?.en}</div>
+						<div>{data?.date?.readable}</div>
 					</div>
-					<div>Jakarta</div>
+					<div>{term}</div>
 				</div>
-				<form
-					onSubmit={(e) => {
-						e.preventDefault();
-						setTerm(input);
-						setInput('');
-					}}>
+				<form onSubmit={handleSubmit}>
 					<input
 						className='bg-white focus:shadow-lg w-[250px] py-2 px-4 rounded-full text-[14px] text-slate-400 outline-0'
 						type='text'
@@ -38,31 +42,23 @@ function App() {
 						value={input}
 						onChange={(e) => setInput(e.target.value)}
 					/>
-					<input type='submit' value='' />
 				</form>
 			</div>
 			<div>
 				<ul className='w-[200px] bg-[#0d0d0d] flex flex-col rounded-xl py-2'>
-					<li className='odd:text-[#26a767] even:text-[#dbdbd9] flex justify-between p-2'>
-						<div>Fajr</div>
-						<div className='font-bold'>04.30</div>
-					</li>
-					<li className='odd:text-[#26a767] even:text-[#dbdbd9] flex justify-between p-2'>
-						<div>Fajr</div>
-						<div className='font-bold'>04.30</div>
-					</li>
-					<li className='odd:text-[#26a767] even:text-[#dbdbd9] flex justify-between p-2'>
-						<div>Fajr</div>
-						<div className='font-bold'>04.30</div>
-					</li>
-					<li className='odd:text-[#26a767] even:text-[#dbdbd9] flex justify-between p-2'>
-						<div>Fajr</div>
-						<div className='font-bold'>04.30</div>
-					</li>
-					<li className='odd:text-[#26a767] even:text-[#dbdbd9] flex justify-between p-2'>
-						<div>Fajr</div>
-						<div className='font-bold'>04.30</div>
-					</li>
+					{data &&
+						prayerTime.map((p) => {
+							return (
+								<li
+									key={p}
+									className='odd:text-[#26a767] even:text-[#dbdbd9] flex justify-between p-2'>
+									<div>{p}</div>
+									<div className='font-bold'>
+										{data && data.timings[`${p}`]}
+									</div>
+								</li>
+							);
+						})}
 				</ul>
 			</div>
 		</div>
