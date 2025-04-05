a466a78919f8ef6917f873fef382074b97df6088
JourneyAries <journeyaries13@gmail.com>
2025-04-05 10:35:26 +0700

    finish[v1]: quiz app

---
+++ quiz-app/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Start from './pages/Start';
+import Question from './pages/Question';
+import Score from './pages/Score';
+import Review from './pages/Review';
+
+function App() {
+	const [display, setDisplay] = useState('start');
+	const [index, setIndex] = useState(0);
+	const [answer, setAnswer] = useState({});
+	const [score, setScore] = useState({});
+	const handlePrev = () => {
+		setIndex((index) => index - 1);
+	};
+	const handleNext = () => {
+		setIndex((index) => index + 1);
+	};
+	const handleReset = () => {
+		setIndex(0);
+		setAnswer({});
+		setDisplay('start');
+		setScore({});
+	};
+	return (
+		<>
+			<div className='h-screen flex flex-col'>
+				<Header display={display} />
+				<div className='w-full bg-charcoal flex-1'>
+					{display === 'start' && <Start setDisplay={setDisplay} />}
+					{display === 'question' && (
+						<Question
+							setScore={setScore}
+							index={index}
+							handleNext={handleNext}
+							handlePrev={handlePrev}
+							answer={answer}
+							setAnswer={setAnswer}
+							setDisplay={setDisplay}
+						/>
+					)}
+					{display === 'score' && (
+						<Score score={score} setDisplay={setDisplay} setIndex={setIndex} />
+					)}
+					{display === 'review' && (
+						<Review
+							index={index}
+							answer={answer}
+							setDisplay={setDisplay}
+							handlePrev={handlePrev}
+							handleNext={handleNext}
+						/>
+					)}
+				</div>
+
+				<Footer handleReset={handleReset} />
+			</div>
+		</>
+	);
+}
+
+export default App;
