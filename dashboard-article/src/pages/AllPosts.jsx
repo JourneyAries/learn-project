0545de25ac05918a63153ba6b6e1a40a5b0615f7
JourneyAries <journeyaries13@gmail.com>
2025-04-11 14:29:33 +0700

    [dashboard-article](v1): tabs all posts 100%

--- dashboard-article/src/pages/AllPosts.jsx
+++ dashboard-article/src/pages/AllPosts.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Navbar from '../components/Navbar';
 
 const AllPosts = () => {
+	const [tab, setTab] = useState('all');
+	const tabs = [
+		{ title: 'All', value: 'all' },
+		{ title: 'Published', value: 'published' },
+		{ title: 'Draft', value: 'draft' },
+		{ title: 'Trashed', value: 'trashed' },
+	];
+
 	return (
 		<div className='app'>
 			<Navbar />
-			<div className='layout'>test</div>
+			<div className='layout'>
+				<div className='header'>
+					<div className='flex-row gap-12'>
+						{tabs.map((item) => {
+							return (
+								<div
+									className={`tab ${tab === item.value ? 'tab-active' : ''}`}
+									onClick={() => setTab(item.value)}>
+									{item.title}
+								</div>
+							);
+						})}
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
